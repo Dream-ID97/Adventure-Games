d974f379944c0091b45dc1fca7523803cb4f59b1
a156f9e0 <dreamlessoriginal8@gmail.com>
2025-01-12 14:40:37 +0800

    Update credit.cpp

---
+++ credit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Fungsi-fungsi sebelumnya tetap sama
+
+void kredit() {
+    cout << "\n-----------------------------------" << endl;
+    cout << "Game Adventure Text" << endl;
+    cout << "Developed by: [a156f9e0]" << endl;
+    cout << "Copyright (C) 2025" << endl;
+    cout << " All copyrights reserved." << endl;
+    cout << "Thanks you for playing!" << endl;
+    cout << "-----------------------------------" << endl;
+}
+
+int main() {
+    cout << "Selamat datang di Game Petualangan Teks!" << endl;
+    cout << "Anda adalah seorang petualang yang mencari rahasia dunia kuno." << endl;
+
+    // Jalur permainan tetap sama
+
+    // Di akhir permainan, tampilkan kredit
+    kredit();
+
+    return 0;
+}
